e400f0e3e97a3c9459a928d6bf82e7417378da82
JangSeHun <westreed@naver.com>
2024-12-06 22:32:19 +0900

    feat: Bump to 0.1.1

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -1,10 +1,9 @@
 import unittest
 from unittest import IsolatedAsyncioTestCase
 
-from chzzk import Chzzk, ChzzkChat
-from chzzk.model import ChatContext
-
 from dotenv import dotenv_values
+
+from chzzk import Chzzk
 
 
 class TestChzzkClient(IsolatedAsyncioTestCase):
@@ -20,24 +19,6 @@ class TestChzzkClient(IsolatedAsyncioTestCase):
 
         for channel in search_channels.data:
             print(await client.channel(channel.channel.channel_id))
-
-    async def test_chzzk_live(self):
-        config = dotenv_values(".env")
-        auth_data = {"auth": config.get("AUTH"), "session": config.get("SESSION")}
-        chzzk = Chzzk.from_data(**auth_data)
-
-        pong = "7ce8032370ac5121dcabce7bad375ced"
-        live = await chzzk.live.status(pong)
-        chat_id = live.chat_channel_id
-
-        print(await chzzk._game_chat.access_token(chat_id))
-
-        chat = ChzzkChat(chzzk=chzzk, chat_context=ChatContext(chat_channel_id=chat_id))
-        await chat.connect()
-        # await chat.run()
-        # await chat.connect()
-
-        print(chat)
 
     async def test_sum(self):
         server_id = sum(map(lambda x: ord(x), list("N1VvHk")))
